064cad4ba8ced67911a0e3949409c0c524177908
rohitkamble7 <rohitkamble.kamble60@gmail.com>
2023-09-20 09:17:18 +0530

    Create and Assign Variable
    Create and Assign Variable

---
+++ 01_variableAssign.js
@@ -0,0 +1,30 @@
+var myFullName
+console.log(myFullName);
+myFullName="Rohit Kamble"
+console.log(myFullName);
+myfullName='Rohit Daji Kamble'
+console.log(myfullName);
+
+
+var myLove='i love only JS'
+console.log(myLove);
+
+var firstName='Rohit'
+console.log("My first name:",firstName);
+var lastName="kamble"
+console.log("My last name:",lastName);
+
+var age="27"
+console.log("Age:",age);
+
+var address="pune"
+console.log("My address:",address);
+
+var pincode="411033"
+console.log("Pincode:",pincode);
+
+var gender="male"
+console.log("Gender:",gender);
+
+var isMarried="Unmarried"
+console.log("Marital Status:",isMarried);
