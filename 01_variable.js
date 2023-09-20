13bd870e9718fe8f6f3c9c5d1a2073f095df7cd6
rohitkamble7 <rohitkamble.kamble60@gmail.com>
2023-09-20 09:16:31 +0530

    First Program Of JS
    First Program Of JS

---
+++ 01_variable.js
@@ -0,0 +1,27 @@
+var firstName; // variable declaration
+
+firstName = "Rohit";  // Variable initialization
+
+console.log(firstName);
+
+firstName = "Mr. Rohit"; // Variable update
+
+console.log(firstName);
+
+var lastName = "kamble"; // variable declaration and initialization
+
+console.log(lastName);
+
+ 
+
+var height; // Variable Declaration
+
+console.log(height);
+
+height = "6"; // initialization
+
+console.log(height);
+
+height = "6.2 feet";// update
+
+console.log(height);
