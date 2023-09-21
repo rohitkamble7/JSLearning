5d9b19332b82fa86502c847211f2abd768cc3e05
rohitkamble7 <rohitkamble.kamble60@gmail.com>
2023-09-21 11:38:28 +0530

    Variable Assignment
    Variable Assignment

---
+++ 02_variableBasicAssignment.js
@@ -0,0 +1,46 @@
+console.log("==============> Assignment No-1 <==================== ");
+
+console.log("===============> Data Type-Number <=================");
+
+var rollNo = 07;
+console.log("My Roll No:-", rollNo, "Data Type:", typeof rollNo);
+
+var accountNo = 202520;
+console.log("Account No:-", accountNo, "Data Type:-", typeof accountNo);
+
+var mobileNo = 123456;
+console.log("Mobile No:-", mobileNo, "Data Types:-", typeof mobileNo);
+
+console.log("----------------------------------------------------");
+
+console.log("=============> Data Type- String <==============");
+var state = "Maharashtra";
+console.log("State:-", state, "Data types:-", typeof state);
+
+var country = "India";
+console.log("Country:-", country, "Data type:-", typeof country);
+
+var book = "atomicHabit";
+console.log("Book:-", book, "Data Type:-", typeof book);
+
+console.log("---------------------------------------------------");
+
+console.log("===============> Data Type-Boolean <===============");
+
+var ispolite = true;
+console.log("is he Polite:-", ispolite, " Data type:-", typeof ispolite);
+
+var isblind = false;
+console.log("is he blind:-", isblind, "Data Type:-", typeof isblind);
+
+var isbest = true;
+console.log(" is she best:-", isbest, "Data Type:-", typeof isbest);
+
+console.log("----------------------------------------------------------");
+
+console.log("================> Null Vs Undefined <=================");
+
+console.log("Undefined Means Value Does Not Assign To variable");
+console.log(
+  "Where as Null Define That Value is assigned but it set to be Null."
+);
