4c4109ed88b5d508855384f326251740170563a0
rohitkamble7 <rohitkamble.kamble60@gmail.com>
2023-09-21 11:37:59 +0530

    Type Of Variable

---
+++ 02_typeOf.js
@@ -0,0 +1,40 @@
+console.log("================> Assignment No-2 <===============");
+
+var bankName;
+console.log("========> Question No-1 <========");
+console.log("Before Initialization:", bankName, "Date Type:-", typeof bankName);
+
+console.log("-------->After Initialization <--------");
+bankName = "SBI Bank";
+console.log("Bank Name:-", bankName, "Data Type:-", typeof bankName);
+console.log("---------------------------------------------------------------");
+
+console.log("========> Question No-2 <========");
+
+var marks = "90%";
+console.log("Marks:-", marks, "Data Types:-", typeof marks);
+
+console.log("--------------------------------------------------------------");
+
+console.log("========>Question No-3<========");
+var isWorking;
+console.log("Before Initialization:-", isWorking, "Data types:-", isWorking);
+
+console.log("--------> After Initialization <--------");
+isWorking = true;
+console.log("Is Bank working:", isWorking, "Data Types:-", typeof isWorking);
+
+console.log("---------------------------------------------------------------");
+
+console.log("=========>Question No-4<=========");
+var totalCount = "Hundred and Seven";
+console.log("------------Question a---------------");
+console.log("Marks:-", totalCount, "Data types:-", typeof totalCount);
+
+console.log("----------Question b----------");
+totalCount = 107;
+console.log("Count:-", totalCount, "Data types:-", typeof totalCount);
+
+console.log(
+  "-------------------------------------------------------------------"
+);
