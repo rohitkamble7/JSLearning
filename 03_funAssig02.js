3de5aefd70a4798022285a4bdf9924ba2bad7ea6
rohitkamble7 <rohitkamble.kamble60@gmail.com>
2023-09-25 16:45:51 +0530

    Basic String Length Findout
    Basic String Length Findout

---
+++ 03_funAssig02.js
@@ -0,0 +1,14 @@
+console.log("===========> Assignment: Normal Function <===========");
+
+function bankDetails(bankName,accountNum,location,pinCode,){
+  console.log("Name Of Bank:",bankName,':::', "Account Number:",accountNum,':::',"Location:",location,':::',"Pincode:",pinCode);
+    
+    console.log("-----------------------------------------------------------------------------------------------");
+    }
+
+bankDetails("CITI Bank", 3456782345,"Pune",431202,);
+bankDetails("Axix Bank",7856782345,"Mumbai",441202);
+bankDetails("HDFC Bank",8956782345,"Pune",631202,)
+
+
+
