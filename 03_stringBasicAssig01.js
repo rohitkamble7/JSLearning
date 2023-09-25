27f44af56791493022ddff9d9fcf762bd42fd418
rohitkamble7 <rohitkamble.kamble60@gmail.com>
2023-09-25 16:47:50 +0530

    String Basic-Assignment
    String Basic-Assignment

---
+++ 03_stringBasicAssig01.js
@@ -0,0 +1,32 @@
+console.log("=====================> String Basic Assignment <=========================");
+
+function stringBasics(){
+
+    console.log("My Dream Company Is Cognizant");
+}
+stringBasics();
+console.log("=============================");
+console.log("-----------------------------step-2---------------------------------------------------------");
+var hobby1="Reading";
+var hobby2="Playing Cricket"
+var hobby3="Learning New Things"
+
+console.log("My Hobbies are---> Hobby-1:",hobby1,':::',"Hobby2:",hobby2,':::',"Hobby-3:",hobby3);
+
+console.log("---------------------------------------------------------------------------------------------");
+var result1= hobby1.length
+console.log("Total Count of Character in Hobby1 is::",result1);
+var result2= hobby2.length
+console.log("Total Count of Character in Hobby1 is::",result2);
+var result3= hobby3.length
+console.log("Total Count of Character in Hobby1 is::",result3);
+
+console.log("------------------------------------------");
+
+var max=hobby1+hobby2+hobby3;
+max.length
+console.log("Sum Of All Hobbies------------------>>>",max.length);
+
+console.log("*******************************************");
+
+
