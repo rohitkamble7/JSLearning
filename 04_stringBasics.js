a557fb811459b8aa21ef8e6ecde10ac7ed9b4808
rohitkamble7 <rohitkamble.kamble60@gmail.com>
2023-09-26 20:21:36 +0530

    Update 04_stringBasics.js

--- 04_stringBasics.js
+++ 04_stringBasics.js
@@ -1,15 +1,12 @@
 var greet = 'Good Morning';
 
- 
-
 console.log("========= length property =============");
-
 var totalLength = greet.length;
-
 console.log('Length of the string is: ', totalLength);
 
- 
+console.log("-----------------------------------------------------------------------");
 
+ 
 console.log("========== charAt() =============== ");
 
 var charAt0 = greet.charAt(0);
@@ -21,7 +18,7 @@ console.log("Character at Index 5 is:",result);
 
 console.log('Char stored at index 5 is: ', greet.charAt(5));
 
- 
+console.log("-------------------------------------------------------------------------------------------"); 
 
 var lastIndexChar = greet.charAt(greet.length-1);
 
@@ -31,7 +28,7 @@ greet.length-3;
 
 var thirdLast=greet.charAt(greet.length-3);
 
-
+console.log("-------------------------------------------------------------------------------------------");
 
 console.log("========== concatenation =============== ");
 
@@ -43,7 +40,7 @@ var result = firstName.concat(lastName);
 
 console.log(result);
 
- 
+console.log("-------------------------------------------------------------------------------------------"); 
 
 console.log("========== indexOf =============== ");
 
@@ -51,22 +48,16 @@ var indexOfM = greet.indexOf('M');
 
 console.log('Index of M is: ', indexOfM);
 
- 
-
- console.log(greet.indexOf('nin'));
-
- 
+console.log(greet.indexOf('nin'));
 
  console.log('Index of char z:',greet.indexOf('z'));
-
- 
 
  console.log('Index of char D:',greet.indexOf('D'));
 
  console.log('Index of char o is:', greet.indexOf('o'));
 
-
- console.log('Index of char o is:', greet.indexOf('o'));
+console.log('Index of char o is:', greet.indexOf('o'));
+console.log("------------------------------------------------------------------------------------");
 
 console.log("========== replace =============== ");
 
@@ -78,7 +69,7 @@ var result = greet.replace(' ', "-");
 
 console.log(result);
 
-
+console.log("-----------------------------------------------------------------------------------------");
 console.log("========== trim() =============== ");
 
 var str = "   Good Afternoon  ";
@@ -96,3 +87,86 @@ var lengthAfterTrim = result.length;
 var result = lengthBeforeTrim - lengthAfterTrim;
 
 console.log('Removed extra spaces are: ', result);
+
+console.log("-------------------------------------------------------------------------------------------");
+
+console.log("========== includes() =============== ");
+
+var result = greet.includes('Morning');
+console.log("Is Morning word available: ", result);
+var result = greet.includes('nin');
+
+console.log("Is nin word available: ", result);
+
+var result = greet.includes('good');
+
+console.log("Is good word available: ", result);
+
+
+console.log("========== search() =============== ");
+
+var greet = "Good Morning";
+
+var result = greet.search('orn');
+
+console.log(result);
+
+var result = greet.search('after');
+
+console.log(result);
+
+
+
+console.log("========== slice() =============== ");
+
+var greet = 'Hello Good Morning!';
+
+var result = greet.slice(6);
+
+console.log(result);
+
+var result = greet.slice(6, 10);
+
+console.log(result);
+
+
+
+console.log("========== split() =============== ");
+
+var result = greet.split(" ");
+
+console.log(result);
+
+ 
+
+//WAP to count the total number words imp interview
+
+var str = "I am UI Developer";
+
+var words = str.split(" ");
+
+console.log(words);
+
+console.log('Total words is: ', words.length);
+console.log(`Total words is: ', ${words.length}`);
+
+// console.log(typeof words);
+
+
+
+
+
+
+// String template
+
+console.log(`I am "UI" Developer `);
+
+var num1 = 10;
+
+var num2 = 20;
+
+var result = num1 + num2;
+
+console.log("Addition of ", num1, "and ", num2, "is :", result);
+
+console.log(` Addition of ${num1} and ${num2}  is: ${result}`);
