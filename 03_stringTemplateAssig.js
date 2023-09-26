1f63d600833aa904bd3179f6b97eafb84908d95e
rohitkamble7 <rohitkamble.kamble60@gmail.com>
2023-09-26 20:20:56 +0530

    String Template Uses
    String Template Uses

---
+++ 03_stringTemplateAssig.js
@@ -0,0 +1,32 @@
+console.log("===============> String Template Assignment <==============");
+
+console.log("---------------------Step-1------------------------------------------------------------");
+
+function stringTemplateAssign(){
+
+console.log(         `My Dream Company Is:  "Congnizant" `);
+}
+stringTemplateAssign();
+console.log(`--------------------------------------------------------------------------------------------`);
+console.log(`
+`);
+
+console.log("---------------------Step-2-----------------------------------------------------------------");
+
+var hobby1= "Reading"
+var hobby2= "Playing Cricket"
+var hobby3= "Learning New things"
+
+console.log(`My Hobbies Are----> !!    ${hobby1}     !!    ${hobby2}      !!      ${hobby3}`);
+console.log(`--------------------------------------------------------------------------------------------`);
+
+var result= hobby1 + hobby2+ hobby3
+console.log(`
+`);
+
+console.log(`----------------------Step-3---------------------------------------------------------------`);
+
+console.log(`Result of String concatination is::: ${result}`);
+
+console.log(`--------------------------------------------------------------------------------------------`);
+
