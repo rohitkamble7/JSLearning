4e2a18f0fb30da5749226befbf72b6e92943f4c8
rohitkamble7 <rohitkamble.kamble60@gmail.com>
2023-09-27 22:35:11 +0530

    Assignment on Operator A

---
+++ 05_operatorA.js
@@ -0,0 +1,69 @@
+console.log("===============Assignment On Operator==================");
+
+function squareOfWordLength(str){
+console.log(`Given String is: "Javascript"`);
+
+var strLength = str.length
+console.log(`Length Of String is: ${strLength}`);
+
+var squareOF= strLength*strLength
+return squareOF
+}
+var result=squareOfWordLength("JavaScript")
+console.log(`Square Of Given String is: ${result}`);
+
+console.log(``);
+
+  function squareOfWordLength1(str2){
+
+    console.log(`Given String is: "Google Chrome"`);
+    var strLength2 = str2.length
+    console.log(`Length Of String is: ${strLength2}`);
+    var strlength2 = strLength2*strLength2
+    return strlength2
+}
+   var result2= squareOfWordLength1("Google Chrome")
+   console.log(`Square Of Given String is: ${result2}`);
+
+   console.log(`
+   `);
+
+   function squareOfWordLength2(str3){
+
+    console.log(`Given String is: "Developer Smart"`);
+    var strLength3 = str3.length
+    console.log(`Length Of String is: ${strLength3}`);
+    var lengthOf3 = strLength3*strLength3
+    return lengthOf3
+}
+   var result3= squareOfWordLength2("Developer Smart")
+   console.log(`Square Of Given String is: ${result3}`);
+
+   console.log(`
+   `);
+
+
+   function course(Skill){
+
+   var lengthCourse= Skill.length
+   console.log( `Length Of String is: ${lengthCourse}`);
+   var divideOf= lengthCourse/4
+   return divideOf
+
+   }
+   var result= course("I am Angular Developer")
+console.log(`Divide String By Number of Word is: ${result}`);
+console.log(`
+`);
+
+function course1(Skill1){
+
+    var lengthCourse2= Skill1.length
+    console.log(`Length Of String is: ${lengthCourse2}`);
+    var multi= lengthCourse2*4
+    return multi
+ 
+    }
+    var result1= course1("I am Angular Developer")
+ console.log(`Multiply String By Number of Word is: ${result1}`);
+
