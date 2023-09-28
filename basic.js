db7e90695d1b81db37b546c19790490f04b76d7c
rohitkamble7 <rohitkamble.kamble60@gmail.com>
2023-09-28 20:06:57 +0530

    Update basic.js

--- basic.js
+++ basic.js
@@ -1,3 +1,80 @@
 
 
 console.log("hello,Rohit");
+
+
+
+var num= "rohit"
+
+var result=Number(num)
+console.log(result);
+
+
+
+function nan(num1){
+
+    var result= Number(num1)
+
+    return result
+   
+
+}
+
+var t= nan("10");
+console.log(t);
+var p=nan("pranit")
+
+console.log(p);
+
+
+var num1="1000"
+
+var result= +num1
+console.log(result);
+
+
+var num2= "Priya";
+
+var result1= +num2
+console.log(result1);
+
+console.log(num2, typeof result1);
+
+
+var num3=1000
+
+var result= String(num3)
+console.log(result);
+
+console.log("num is",num3,typeof result );
+
+
+
+
+var name="Rohit";
+
+var result= +name
+console.log(result);
+
+
+
+var name6="priya"
+
+var result= +name6
+console.log("hey",result);
+
+console.log(`
+
+
+
+
+
+`);
+
+
+var num2= "Priya";
+
+var result1= +num2
+console.log(result1);
+
+console.log(num2, typeof result1);
