fd49e2520fcbe0a1063e4f7441ad7f0e1eba4e69
rohitkamble7 <rohitkamble.kamble60@gmail.com>
2023-09-30 00:09:08 +0530

    Explained

---
+++ 06_ifStatement.js
@@ -0,0 +1,74 @@
+var girlAge = 19;
+
+if (girlAge>=18) {
+
+    console.log(`You could go inside office`);
+
+    console.log(`Have you informed your parent`);
+
+    console.log(`Signed.. Congrats!!!`);
+
+}
+
+console.log(`Daily life`);
+
+ 
+
+var elonAge = 17;
+
+ 
+
+// If age is greater than equal 18 then allow for vote else no.
+
+if (elonAge>=18) {
+
+    console.log(`Welcome.. Elon`);
+
+    console.log(`Please let me mark your finger with ink`);
+
+    console.log(`Please now vote`);
+
+} else {
+
+    console.log(`Yet you are not eligible for vote`);
+
+    console.log(`Try next time please!`);
+
+}
+
+console.log(`==================`);
+
+
+var str=" i love my country"
+
+var length= str.length
+
+var result= length*length
+console.log(length);
+console.log(result);
+
+
+var rectangle=function(length,width){
+
+var result= length*width
+
+console.log(result);
+}
+rectangle(499,917);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(typeof rectangle );
