40a5b35f3d6ba0afa247a5a30646b5f44ecc4ac0
rohitkamble7 <rohitkamble.kamble60@gmail.com>
2023-10-02 15:23:27 +0530

    Assignment -On For Loop
    Assignment -On For Loop

---
+++ 06_for_Assignment.js
@@ -0,0 +1,57 @@
+console.log(`===============Assignment===================`);
+
+
+var result= ""  
+for (let index = 5; index <=15 ; index=index+1) {
+  result= result + index+ " "
+
+}
+console.log(result);
+
+console.log(`--------------------------------------------------`);
+
+
+
+var result=""
+for (let index = 50; index>=40; index=index-1) {
+   result= result + index+ " "
+    
+}
+console.log(result);
+
+console.log(`----------------------------------------------------`);
+
+
+var str=""
+for (let index = 1; index<=30 ; index=index+2) {
+    str= str+ index + " "
+    
+}
+console.log(str);
+
+console.log(`-----------------------------------------------------------`);
+
+for (let index = 2; index <=20; index+=2) {
+ console.log(index);
+    
+}
+
+for (let index = 5; index <=50; index+=5) {
+    console.log(index);
+    
+}
+console.log(`---------------------------------------------------------------`);
+
+for (let index = 10; index <=100; index+=10) {
+    console.log(index);
+    
+}
+
+console.log(`-------------------------------------------------------------------`);
+
+for (let index = 100; index>=10; index-=10) {
+    console.log(index);
+    
+}
+
+
