d75d209d8531967c9adb995f6bd95e708d0342a2
rohitkamble7 <rohitkamble.kamble60@gmail.com>
2023-10-02 23:42:25 +0530

    Little Change- Add Space b/w Output
    Little Change- Add Space b/w Output

--- 06_for_Assignment.js
+++ 06_for_Assignment.js
@@ -6,6 +6,7 @@ for (let index = 5; index <=15 ; index=index+1) {
   result= result + index+ " "
 
 }
+console.log(` || Numbers From 5 to 15 incerment by 1 ||`);
 console.log(result);
 
 console.log(`--------------------------------------------------`);
@@ -17,6 +18,7 @@ for (let index = 50; index>=40; index=index-1) {
    result= result + index+ " "
     
 }
+console.log(` || Numbers From 50 to 40 decrement by 1 ||`);
 console.log(result);
 
 console.log(`----------------------------------------------------`);
@@ -27,31 +29,49 @@ for (let index = 1; index<=30 ; index=index+2) {
     str= str+ index + " "
     
 }
+console.log(`|| First 15 Odd Numbers ||`);
 console.log(str);
 
 console.log(`-----------------------------------------------------------`);
 
+var result=""
 for (let index = 2; index <=20; index+=2) {
- console.log(index);
-    
-}
+ result= result+ index+ " "
 
-for (let index = 5; index <=50; index+=5) {
-    console.log(index);
     
 }
+console.log(`|| First 10 Even Numbers ||`);
+console.log(result);
+
+console.log(`-----------------------------------------------------------------------`);
+
+var result=""
+for (let index = 5; index <=50; index+=5) {
+    result= result+ index+ " "
+    
+}
+console.log(` || Table Of 5 ||`);
+console.log(result);
+
 console.log(`---------------------------------------------------------------`);
 
+var result= ""
+
 for (let index = 10; index <=100; index+=10) {
-    console.log(index);
+    result= result+ index+ " "
     
 }
+console.log(` || Table Of 10 ||`);
+console.log(result);
 
 console.log(`-------------------------------------------------------------------`);
 
+var result=""
 for (let index = 100; index>=10; index-=10) {
-    console.log(index);
+    result= result+ index+ " "
     
 }
+console.log(`|| Table Of 10 In Reverse Order ||`);
+console.log(result);
 
 
