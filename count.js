b06c3d0df8ce0c1a6f0dc41b12856ce19ad4c7a0
rohitkamble7 <rohitkamble.kamble60@gmail.com>
2023-10-11 00:06:59 +0530

    Assignment On count
    Assignment On count

---
+++ count.js
@@ -0,0 +1,17 @@
+console.log(`============Assignment - Count Character===========`);
+function countCharA(arg1){
+    console.log(`The Character present in the given string== ${arg1}`);
+var str1 =arg1;
+var count = 0;
+    for (let index = 0; index < str1.length-1; index++) {
+        var char = str1.charAt(index);
+        if (char=='A' || char=='a') {
+            count = count + 1;
+        }
+    }
+    console.log(`Total Number of Character 'A' & 'a' present in Given String : ${count}`) ;
+}
+countCharA('I AM Learning Javascript, The Language of internet');
+console.log(`---------------------------------------------------------------------------------------------------`);
+countCharA('My favourite movie is LAggAn');
+console.log(`---------------------------------End--------------------------------`);
