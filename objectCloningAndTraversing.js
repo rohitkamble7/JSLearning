9b2132f8ab0f5ca0f2a86bf3141e48f499c4f025
rohitkamble7 <rohitkamble.kamble60@gmail.com>
2023-10-12 23:53:01 +0530

    Assignment No Object Traverse
    Assignment No Object Traverse

---
+++ objectCloningAndTraversing.js
@@ -0,0 +1,57 @@
+
+
+
+let bankSbi = {
+
+    branchAddress: "Pune",
+    branchCode: "SBI007",
+    ifscCode: "SBI0008",
+    Email: "www.sbi.com"
+
+};
+
+
+let bankLocation= {
+
+    street: "S.B Patil Road",
+    city: "PimpriChinchwad",
+    pincode: 411033
+}
+
+let mergeDetails= Object.assign( bankSbi,bankLocation)
+
+console.table(mergeDetails);
+console.log(`---------------------------------------------------------------------------`);
+
+console.log(`Cloning Object By Spread operator`);
+
+bankSbi = { ...bankLocation};   
+
+console.log(bankSbi);
+
+console.log(`----------------------------------------------------------------------------`);
+
+let rateOfInterest = {
+
+    homeLoanInterest: '8%',
+    personalLoanInterest: '12%',
+    dueInterest: '10%' ,
+
+
+};
+
+let sbiDetails= Object.assign(bankSbi,bankLocation,rateOfInterest)
+
+console.table(sbiDetails);
+
+console.log(`--------------------------------------------------------------------------------`);
+
+
+for (const key in sbiDetails) {
+     {
+        const element = sbiDetails[key];
+
+        console.log(` ${key}  :: ${element}`);
+        
+    }
+}
