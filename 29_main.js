46840793f289d21c516bd27bcc99c65f5cda88e5
rohitkamble7 <rohitkamble.kamble60@gmail.com>
2023-10-24 21:27:05 +0530

    Detail Explanation
    Detail Explanation

---
+++ 29_main.js
@@ -0,0 +1,16 @@
+
+
+
+import PI from "./29_module.js"; // Default export
+
+import {arrayNum, sum} from "./29_module.js"; // Named Export 
+
+ 
+
+console.log(PI);
+
+console.log(arrayNum);
+
+ 
+
+sum(10, 20);
